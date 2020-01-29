7f29172a365e78c0c70bfcf027b7e96809b2cdb5
thanakit12.40 <thanakit.40@gmail.com>
2020-01-29 16:46:11 +0700

    edit ui in chekname app

--- LoginScreen.js
+++ LoginScreen.js
@@ -13,6 +13,7 @@ import {
     Image,
     TouchableHighlight
   } from 'react-native';
+// import ku_logo from './android/statics/images/ku_logo.png'
 
 export default class LoginScreen extends Component {
     constructor(props) {
@@ -50,31 +51,40 @@ export default class LoginScreen extends Component {
     render() {
       return (
         <View style={styles.container}>
+          <Image style={styles.CustomImg} source={require('./android/statics/images/ku_logo.png')}/>
+          <View style={{ height: 16 }} />
+          <Text style={styles.LabelTitle}>
+            CHECK NAME
+          </Text>
+          <View style={{ height: 12 }} />
+          <Text style={styles.LabelText}>
+            YOUR EMAIL :
+          </Text>
           <View style={styles.inputContainer}>
-            <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
             <TextInput style={styles.inputs}
                 placeholder="Email"
                 keyboardType="email-address"
                 underlineColorAndroid='transparent'
                 onChangeText={(email) => this.setState({email})}/>
           </View>
-          
+          <Text style={styles.LabelText}>
+            PASSWORD :
+          </Text>
           <View style={styles.inputContainer}>
-            <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
             <TextInput style={styles.inputs}
                 placeholder="Password"
                 secureTextEntry={true}
                 underlineColorAndroid='transparent'
                 onChangeText={(password) => this.setState({password})}/>
           </View>
-  
+
+          <TouchableHighlight style={styles.buttonForget} onPress={() => this.onClickListener('restore_password')}>
+              <Text style={{ textDecorationLine: 'underline'}}>Forgot your password?</Text>
+          </TouchableHighlight>
+
           <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} 
           onPress={this.Event_login}>
             <Text style={styles.loginText}>Login</Text>
-          </TouchableHighlight>
-  
-          <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
-              <Text>Forgot your password?</Text>
           </TouchableHighlight>
   
           <TouchableHighlight style={styles.buttonContainer} onPress={() => this.props.navigation.navigate('Register')}>
@@ -90,7 +100,9 @@ export default class LoginScreen extends Component {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#DCDCDC',
+      backgroundColor: '#FFFFFF',
+      fontFamily: 'Kanit',
+      fontStyle: 'normal',
     },
     inputContainer: {
         borderBottomColor: '#F5FCFF',
@@ -101,33 +113,68 @@ export default class LoginScreen extends Component {
         height:45,
         marginBottom:20,
         flexDirection: 'row',
-        alignItems:'center'
+        alignItems:'center',
+        borderColor: '#949AA0',
+        borderWidth: 1,
     },
     inputs:{
-        height:45,
+        height: 45,
         marginLeft:16,
         borderBottomColor: '#FFFFFF',
         flex:1,
+        
     },
     inputIcon:{
       width:30,
       height:30,
       marginLeft:15,
-      justifyContent: 'center'
+      justifyContent: 'center',
     },
     buttonContainer: {
-      height:45,
+      height: 45,
       flexDirection: 'row',
       justifyContent: 'center',
       alignItems: 'center',
       marginBottom:20,
-      width:250,
+      width: 150,
       borderRadius:30,
     },
+    buttonForget: {
+      height: 25,
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      marginBottom: 20,
+      width: 250,
+    },
     loginButton: {
-      backgroundColor: "#00b5ec",
+      backgroundColor: "#CA5353",
     },
     loginText: {
       color: 'white',
+    },
+    CustomImg:{
+      width: 90,
+      height: 116,
+      top: 20,
+
+    },
+    LabelTitle: {
+      fontWeight: 'bold',
+      fontSize: 28,
+      lineHeight: 42,
+      display: 'flex',
+      alignItems: 'center',
+      textAlign: 'center',
+      color: 'rgba(0, 0, 0, 0.67)',
+    },
+    LabelText: {
+      fontWeight: 'normal',
+      fontSize: 14,
+      lineHeight: 21,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      width: 250,
     }
   });
